50c842574efa6f5671edde97d86ca5e90b88ab4e
Gustavo780904 <gustavodutrasantos@gmail.com>
2021-06-26 14:47:47 -0300

    teste

---
+++ Composite/src/pancake/PancakeHouseMenuIterator.java
@@ -0,0 +1,51 @@
+package pancake;
+
+import java.util.ArrayList;
+
+import interfaceMenu.Iterator;
+import items.MenuItem;
+
+public class PancakeHouseMenuIterator implements Iterator {
+	ArrayList<MenuItem> items;
+	
+	public PancakeHouseMenuIterator() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public PancakeHouseMenuIterator(ArrayList<MenuItem> items) {
+		super();
+		this.items = new ArrayList<>();
+	}
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+//
+//	public boolean hasNext() {
+//
+//		return false;
+//	}
+//
+//	public Object next(ArrayList<MenuItem> items) {
+//	
+//			
+//		return false;
+//	}
+
+	
+
+
+
+	
+
+}
